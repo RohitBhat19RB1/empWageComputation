c0f3cd8b511c5c567bb7d6e2bfa92ae692d5d976
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-08-07 20:59:08 +0530

    modified file in usecase3

--- empWageComp.sh
+++ empWageComp.sh
@@ -1,16 +1,21 @@
 #!/bin/bash -x
 #echo "Welcome to Employee Wage Computation program"
 
-isPresent=1;
-randomCheck=$((RANDOM%2));
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
 
-if [ $isPresent -eq $randomCheck ];
+if [ $isFullTime -eq $randomCheck ];
 then
-        empRatePerHr=20;
-	empHrs=8;
-	salary=$(($empHrs*$empRatePerHr));
-
+        empHrs=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+        empHrs=4;
 else
-        salary=0;
+        empHrs=0;
 fi
+
+salary=$(($empHrs*$empRatePerHr));
+
 
